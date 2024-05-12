1204e253d5763cea7dd245a8f28676dea4b89133
lukebellis <contact@lukeellis.me>
2024-05-12 18:22:53 +0100

    smoother sudo

--- install_dev_tools.sh
+++ install_dev_tools.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Request sudo password upfront
+sudo -v
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Update the package list
 echo "Updating package list..."
 sudo apt update
